8d2f2d99751a7f97c82140184cafd7b761e6874e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-10 17:17:58 -0700

    Restaurant ratings function rewritten with single use functions

--- ratings.py
+++ ratings.py
@@ -5,7 +5,7 @@
 
 import sys
 
-def get_restaurant_ratings(input_file):
+def make_restaurant_ratings_dict(input_file):
     ratings_dict = {}
 
     file = open(input_file)
@@ -13,15 +13,43 @@ def get_restaurant_ratings(input_file):
         restaurant_name, rating = line.rstrip().split(":")
         ratings_dict[restaurant_name]= rating
     
+    return ratings_dict
+
+    # new_restaurant = input("What is your restaurant name?: ").title()
+    # new_rating = input("What do you rate your restaurant on a scale of 1 to 5?: ")
+
+    # ratings_dict[new_restaurant] = new_rating
+
+    # sorted_restaurants_list = sorted(ratings_dict)
+
+    # for restaurant in sorted_restaurants_list:
+    #     rating = ratings_dict[restaurant]
+    #     print("{} is rated at {}.".format(restaurant, rating))
+
+
+def add_new_restaurant(ratings_dict):
     new_restaurant = input("What is your restaurant name?: ").title()
     new_rating = input("What do you rate your restaurant on a scale of 1 to 5?: ")
 
     ratings_dict[new_restaurant] = new_rating
+    return ratings_dict
 
+
+def sort_and_print_restaurants(ratings_dict):
     sorted_restaurants_list = sorted(ratings_dict)
 
     for restaurant in sorted_restaurants_list:
         rating = ratings_dict[restaurant]
         print("{} is rated at {}.".format(restaurant, rating))
 
-get_restaurant_ratings(sys.argv[1])
+
+def rating_restaurants(input_file):
+
+    ratings_dictionary = make_restaurant_ratings_dict(input_file)
+
+    ratings_dictionary = add_new_restaurant(ratings_dictionary)
+
+    sort_and_print_restaurants(ratings_dictionary)
+
+
+rating_restaurants(sys.argv[1])
